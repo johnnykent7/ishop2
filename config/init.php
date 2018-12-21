4366e54627075d74bcf38221cb10fa58d1d33945
Ion Glavan <ionelglavan@gmail.com>
2018-12-21 12:18:41 +0200

    added based routes descb

---
+++ config/init.php
@@ -0,0 +1,26 @@
+<?php 
+    define("DEBUG", 1);
+    define("ROOT", dirname(__DIR__));
+    define("WWW", ROOT . '/public');
+    define("APP", ROOT . '/app');
+    define("CORE", ROOT . '/vendor/ishop/core');
+    define("LIBS", ROOT . '/vendor/ishop/core/libs');
+    define("CACHE", ROOT . '/tmp/cache');
+    define("CONF", ROOT . '/config');
+    define("LAYOUT", 'defaoult');
+
+    //http://localhost/ishop2.loc/public/index.php
+    $app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+
+    //http://localhost/ishop2.loc/public/
+    $app_path = preg_replace( "#[^/]+$#", '', $app_path);
+
+    //http://localhost/ishop2.loc/
+    $app_path = str_replace( '/public/', '', $app_path);
+
+    define("PATH", $app_path);
+
+    define("ADMIN", PATH . '/admin');
+
+    require ROOT . '/vendor/autoload.php';
+    ?>
